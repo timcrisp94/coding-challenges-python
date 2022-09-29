f508ae8ac0489fd2cab04349a5052c9e05ba87f4
Tim Crisp <timothycrisp@gmail.com>
2022-09-29 09:00:16 -0500

    solve valid anagram using two hashes

---
+++ leetcode/week-one/valid-anagram.py
@@ -0,0 +1,32 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# an Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+# Example 1:
+
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+# Example 2:
+
+# Input: s = "rat", t = "car"
+# Output: false
+
+# - pseudo - 
+# function(s, t)
+# hash = {}
+# edge - if s.len !== t.len 
+# for char of s
+#   add to hash
+# for char of t
+#   if !hash[char]
+
+class Solution:
+  def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+      return False
+
+    countS, countT = {}, {}
+    for i in range(len(s)):
+      countS[s[i]] = 1 + countS.get(s[i], 0)
+      countT[t[i]] = 1 + countT.get(t[i], 0)
+    return countS == countT
