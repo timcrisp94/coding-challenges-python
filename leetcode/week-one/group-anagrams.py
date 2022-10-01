f5fa9b0b1cb59b09117c6340625b8ec42ad21eff
Tim Crisp <timothycrisp@gmail.com>
2022-10-01 09:18:27 -0500

    optimize group anagrams run-time

--- leetcode/week-one/group-anagrams.py
+++ leetcode/week-one/group-anagrams.py
@@ -26,19 +26,33 @@
 # return ans.values()
 
 
+# class Solution(object):
+#   def groupAnagrams(self, strs):
+#     """
+#     :type strs: List[str]
+#     :rtype: List[List[str]]
+#     """
+#     result = defaultdict(list)
+
+#     for s in strs:
+#       count = [0] * 26
+#       for c in s:
+#         count[ord(c) - ord("a")] += 1
+#       result[tuple(count)].append(s)
+#     return result.values()
+    
+
 class Solution(object):
   def groupAnagrams(self, strs):
     """
     :type strs: List[str]
     :rtype: List[List[str]]
     """
-    result = defaultdict(list)
+    dic = defaultdict(list)
+    for str in strs:
+      s = ''.join(sorted(str))
+      dic[s].append(str)
 
-    for s in strs:
-      count = [0] * 26
-      for c in s:
-        count[ord(c) - ord("a")] += 1
-      result[tuple(count)].append(s)
-    return result.values()
+    return dic.values()
 
 # time : O(m * n)
